3fcb8feb5fc4eefad71dbbe77b5c3a6a600a810d
vfeofanov <vasya3000-95@mail.ru>
2024-10-30 15:13:07 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='mantis',
-    version='0.1.0',  # Start with an initial version
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    packages=find_packages(),
-    # Other metadata like author, description, etc.
-)
+# For retrocompatibility with pip < 22 in editable mode
+setup()
